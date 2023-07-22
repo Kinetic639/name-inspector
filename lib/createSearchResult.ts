b2ecf5142b961deef2781bd84702a657ee89f818
Kinetic639 <michal.stepien36@gmail.com>
2023-07-22 18:28:08 +0200

    checking api prod url

--- lib/createSearchResult.ts
+++ lib/createSearchResult.ts
@@ -1,10 +1,13 @@
 import { SearchResult } from "@/types";
 
 export default async function (searchResult: SearchResult) {
-  const res = await fetch(`${process.env.apiBaseUrl}/api/search-result`, {
-    method: "POST",
-    body: JSON.stringify(searchResult),
-  });
+  const res = await fetch(
+    `https://name-inspector-kinetic639.vercel.app/api/search-result`,
+    {
+      method: "POST",
+      body: JSON.stringify(searchResult),
+    },
+  );
   if (!res.ok) throw new Error("Failed to save results to the databe");
   return res.json();
 }
