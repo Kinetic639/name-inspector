a28438cfb673065157a03ca6efb04ccaf3f9c55e
Kinetic639 <michal.stepien36@gmail.com>
2023-07-26 11:01:52 +0200

    Added content to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <main className="overflow-hidden">home</main>;
+  return (
+    <main className="flex flex-col items-center mt-8">
+      <h1 className="text-3xl uppercase font-semibold mb-14">Home Page</h1>
+      <ul className="list-none">
+        <li className="text-lg">
+          Use username: <span className="font-semibold">admin</span> and
+          password: <span className="font-semibold">admin</span> to log in to
+          the app and view history.
+        </li>
+      </ul>
+    </main>
+  );
 }
